055845b7349a02636360d3a05e397af0d0ab82fc
Julião Kataleko <juliofeli78@gmail.com>
2023-10-19 18:07:45 +0100

    feat: fist commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div>
+            Message: 
+
+            <div v-for="message in messages" :key="message">
+                <div>{{ message }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                messages: []
+            }
+        },
+        created() {
+            console.log('Component created.')
+
+            Echo.channel('notification').listen('MessageNotification', (e) => {
+                console.log("Mensagem recebida...");
+
+                console.log(e);
+                this.messages.push(e.message)
+                
+            })
+        },
+        mounted() {
+
+            // Echo.channel('notification').listen(
+            //     'MessageNotification',
+            //     (e) => {
+            //         console.log("Mensagem recebida...");
+            //     }
+            // )
+        }
+    }
+</script>
